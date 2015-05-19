fae2aaa052186c1853b25ac7578b18eda69270ff
lskaye <lskaye@mxschool.edu>
2015-05-19 17:33:31 -0400

    Final Demos

---
+++ SCRIPTDEMO/script/script.js
@@ -0,0 +1,18 @@
+alert("Hello");
+alert("Today is Saturday!");
+
+var current = 4 * 2;
+if(current < 9) {
+	alert(current + " < 9");
+}
+else {
+	alert(current + " >= 9");
+};
+
+for (var i = 0; i < 12; i = i +3)
+{
+	current = current + i;
+};
+if (current > 2) {
+	alert("The new value of current is: " + current)
+};
